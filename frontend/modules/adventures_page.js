ec9dc49490f3ea3e90af8b18b7d992315952a2d0
CrioUser <crio-user@criodo.com>
2023-03-02 12:39:39 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURES

--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -3,8 +3,14 @@ import config from "../conf/index.js";
 
 //Implementation to extract city from query params
 function getCityFromURL(search) {
+  let params=new URLSearchParams(search);
+  let urlCity= params.get('city');
+  console.log(urlCity);
+  return urlCity;
+  
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
+
 
 }
 
@@ -12,14 +18,84 @@ function getCityFromURL(search) {
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
+  const url =config.backendEndpoint+"/adventures?city="+city;
+  try{
+    let res = await fetch(url);
+  
+    let data = await res.json();
+    return data;
+  }
+  catch(error){
+    return null;
+  }
+
 
 }
 
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
 function addAdventureToDOM(adventures) {
+  
+// File: pages/adventures/index.html
+let row=document.getElementById("data");
+
+for(let adventure of adventures)
+{
+const divCol=document.createElement("div");
+divCol.setAttribute("class","col-6 col-lg-3 mb-3");
+
+const anchor=document.createElement("a");
+let url="detail/?adventure="+adventure.id;
+anchor.setAttribute("href",url);
+anchor.id=adventure.id;
+
+const divCard=document.createElement("div");
+divCard.setAttribute("class","activity-card");
+
+const img=document.createElement("img");
+img.src=adventure.image;
+img.alt=adventure.name+"image";
+
+const divBan=document.createElement("div");
+divBan.setAttribute("class","category-banner");
+divBan.textContent=adventure.category;
+
+let container1=document.createElement("div");
+container1.setAttribute("class","activity-card-text");
+let divName=document.createElement("div");
+divName.textContent=adventure.name;
+let divPrice=document.createElement("div");
+divPrice.textContent="₹"+adventure.costPerHead;
+container1.appendChild(divName);
+container1.appendChild(divPrice);
+
+
+let container2=document.createElement("div");
+container2.setAttribute("class","activity-card-text");
+let divDur=document.createElement("div");
+divDur.textContent="Duration";
+let divDuration=document.createElement("div");
+divDuration.textContent=adventure.duration +"Hours";
+container2.appendChild(divDur);
+container2.appendChild(divDuration);
+
+const divContent=document.createElement("div");
+
+divContent.appendChild(container1);
+divContent.appendChild(container2);
+
+divCard.appendChild(img);
+divCard.appendChild(divBan);
+divCard.appendChild(divContent);
+
+anchor.appendChild(divCard);
+
+divCol.appendChild(anchor);
+
+row.appendChild(divCol);
+
+}
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
-
 }
 
 //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
